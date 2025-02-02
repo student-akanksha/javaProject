765b987ff4c945776ae05a1ef640fd5c51afbabf
prem-syntasa <prem.chand@syntasa.com>
2025-02-02 21:01:02 +0530

    update practice file array

--- practicePogram.java
+++ practicePogram.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class practicePogram {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        String[] cars = { "Volvo", "BMW", "Ford", "Mazda", "asd" };
+        String[] cars = { "Volvo", "BMW", "Ford", "Mazda"};
         for (int i = 0; i < cars.length; i++) {
             System.out.println(cars[i]);
         }
